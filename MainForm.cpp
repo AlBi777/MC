7b2d275cf244607d54785d0258b6c5940f45df44
AlexanderBipert <aleksandrbi777@gmail.com>
2022-12-06 21:22:30 +0700

    Добавьте файлы проекта.

---
+++ MainForm.cpp
@@ -0,0 +1,15 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+
+void main() {
+
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	MC::MainForm form;
+	Application::Run(% form);
+}
